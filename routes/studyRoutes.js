19b071b4249bc528c4a36075d4acc2165930529a
Muhaiminul Mugdha <muhaiminulmugdha@gmail.com>
2026-09-09 13:02:04 -0400

    feat(routes): map endpoints for editing and deleting courses and lecture logs

--- routes/studyRoutes.js
+++ routes/studyRoutes.js
@@ -4,6 +4,11 @@ const studyController = require('../controllers/studyController');
 
 router.get('/', studyController.getStudyPage);
 router.post('/courses', studyController.createCourse);
+router.post('/courses/:courseId/update', studyController.updateCourse); // <-- Edit Course
+router.post('/courses/:courseId/delete', studyController.deleteCourse); // <-- Delete Course
+
 router.post('/courses/:courseId/lectures', studyController.createLecture);
+router.post('/courses/:courseId/lectures/:lectureId/update', studyController.updateLecture); // <-- Edit Lecture
+router.post('/courses/:courseId/lectures/:lectureId/delete', studyController.deleteLecture); // <-- Delete Lecture
 
 module.exports = router;
